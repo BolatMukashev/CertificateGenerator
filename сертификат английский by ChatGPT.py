ab6d34e26e9e14eca13a65894248db1b197ef8f2
Bolat Mukashev <ya.ne.angel.kimi@gmail.com>
2023-07-03 17:36:40 +0500

    first commit

---
+++ сертификат английский by ChatGPT.py
@@ -0,0 +1,61 @@
+import os
+from enum import Enum
+from pathlib import Path
+from PIL import Image, ImageFont, ImageDraw
+
+
+# path = "C:/Windows/Fonts"
+# fonts = os.listdir(path)
+# print(fonts)
+
+
+class CoursesTypes(Enum):
+    PRE_INTERMEDIATE = 'has completed Pre-intermediate English Course'
+    ELEMENTARY = 'has completed Elementary English Course'
+    ELEMENTARY_ONLINE = 'has completed Elementary (online) English Course'
+    BEGINNERS = 'has completed Beginners English Course'
+    WAY_AHEAD = 'has completed Way Ahead English Course'
+    FIRST = 'has completed First English Course'
+    ENJOY = 'has completed Enjoy English Course'
+
+
+class CertificateGenerator:
+    def __init__(self, template_path, font_to_name, font_to_text, output_path):
+        self.template_path = template_path
+        self.font_to_name = font_to_name
+        self.font_to_text = font_to_text
+        self.output_path = output_path
+
+    def generate_certificate(self, name, course, date):
+        with Image.open(self.template_path) as img:
+            resized_image = img.resize((3507, 2480), resample=Image.Resampling.LANCZOS)
+            draw = ImageDraw.Draw(resized_image)
+            font_to_name = ImageFont.truetype(self.font_to_name, 120)
+            font_to_text = ImageFont.truetype(self.font_to_text, 72)
+            draw.text((1750, 1240), name, (51, 98, 105), font=font_to_name, anchor='mb')
+            draw.text((1770, 1350), course, (51, 98, 105), font=font_to_text, anchor='mb')
+            draw.text((1100, 2038), date, (51, 98, 105), font=font_to_text, anchor='ms')
+            resized_image.save(Path(self.output_path, name + '.jpg'), 'JPEG', quality=100, dpi=(300, 300))
+
+with open('names.txt', 'r') as file:
+    names = file.read().splitlines()
+
+course = CoursesTypes.ELEMENTARY_ONLINE
+date = "June 2023"
+
+save_path = Path("C:/Users/bolat/Desktop/Client/", course.name)
+save_path.mkdir(parents=True, exist_ok=True)
+
+if len(names) == 1:
+    names = names[0].split(', ')
+
+
+certificate_generator = CertificateGenerator(
+    template_path="C:/Users/bolat/Desktop/my_programs/CertificateGenerator/Certificate english английский.jpg",
+    font_to_name="C:/Windows/Fonts/Cassandra.ttf",
+    font_to_text="C:/Windows/Fonts/ebrima.ttf",
+    output_path=save_path
+)
+
+for name in names:
+    certificate_generator.generate_certificate(name, course.value, date)
